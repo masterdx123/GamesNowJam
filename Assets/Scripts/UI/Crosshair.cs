378c06559dc69862e6c167c9ab7c708d6e27364e
Lucas Patrício <62174653+LucasPatricio3@users.noreply.github.com>
2024-11-17 19:49:22 +0000

    Crosshair

--- Assets/Scripts/UI/Crosshair.cs
+++ Assets/Scripts/UI/Crosshair.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
-[RequireComponent(typeof(SpriteRenderer))]
 public class Crosshair : MonoBehaviour
 {
     private PlayerController _player;
+    [SerializeField] private GameObject GameManagerCanvas;
     private void Start()
     {
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         _player.OnPlayerDeath += () => Cursor.visible = true;
         Cursor.visible = false;
+        this.transform.parent = GameManagerCanvas.transform;
     }
     // Update is called once per frame
     void LateUpdate()
     {
-        var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - transform.position);
+        var mouseWorldPos = Input.mousePosition;
         this.transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, 0);
     }
 }
